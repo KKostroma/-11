a237a77f7c6dfc02825301be13cf11f3ae855d06
Ekaterina Kostromitskaia <kostroma.mi@mail.ru>
2021-12-05 23:23:08 +0300

    Добавьте файлы проекта.

---
+++ Задание 11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab_11;
+
+namespace Задание_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Yravnenie yravnenie = new Yravnenie(1, 2);
+            yravnenie.Root();
+            Console.ReadKey();
+        }
+    }
+}
